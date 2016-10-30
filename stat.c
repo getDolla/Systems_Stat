2f1dc8f359e09e1c4d729211dc8989c1a88cfaf7
Wilson Berkow <wilsonberkow@gmail.com>
2016-10-30 12:51:23 -0400

    Use filestat on the stack (remove malloc)

--- stat.c
+++ stat.c
@@ -12,32 +12,30 @@ void printStat( char * file ){
         printf( "No filename entered!" );
         return;
     }
-    
-    struct stat *filestat = (struct stat*) malloc (sizeof(struct stat));
-    
-    if( stat( file, filestat ) == 0 )
-    { 
+
+    struct stat filestat;
+
+    if( stat( file, &filestat ) == 0 )
+    {
         printf( "File: %s\n", file ); //name
-        printf("File Size: %u bytes\n", filestat->st_size ); //size
-        printf("Access Permissions of file: %o\n", filestat->st_mode); //permissions
-        printf("Time last accessed: %s\n", ctime( &(filestat->st_atime) )); //time last accessed
-    } 
-    else
+        printf("File Size: %u bytes\n", filestat.st_size ); //size
+        printf("Access Permissions of file: %o\n", filestat.st_mode); //permissions
+        printf("Time last accessed: %s\n", ctime( &(filestat.st_atime) )); //time last accessed
+    }
+    else {
         printf("File Error %d: %s\n", errno, strerror(errno));
-    
-    free( filestat );        
+    }
 }
- 
+
 void main()
-{ 
+{
     //used to test other files
     /*
-    // Get file name 
+    // Get file name
     printf("Enter Filename....\n");
     scanf( "%s", file );
     */
-    
+
     printStat( "file.txt" ); //print info on default file
- 
-     
+
 }
