5be7453f9bb9a85a536e519071183c835d44b413
Wilson Berkow <wilsonberkow@gmail.com>
2016-10-31 11:50:23 -0400

    Pretty-print file size

--- stat.c
+++ stat.c
@@ -38,6 +38,34 @@ char* printPermissions( int n ) {
   return c;
 }
 
+char handle_prefix(long int size, long int * ret) {
+    long int s = size;
+    while (s >= 1000) {
+        s /= 1000;
+    }
+    *ret = s;
+    if (size < 1e6) return 'k';
+    if (size < 1e9) return 'M';
+    if (size < 1e12) return 'G';
+    if (size < 1e15) return 'T';
+    if (size < 1e18) return 'P';
+    if (size < 1e21) return 'E';
+    if (size < 1e24) return 'Z';
+    if (size < 1e27) return 'Y';
+    return '!';
+}
+
+void fmt_SI(long int size, char unit_char, char* buf) {
+    if (size < 1000) {
+        sprintf(buf, "%li B\0", size);
+    } else if (size < 1e27) {
+        long int displayval;
+        char prefix = handle_prefix(size, &displayval);
+        sprintf(buf, "%li %c%c\0", displayval, prefix, unit_char);
+    } else {
+        sprintf(buf, "%li Y%c\0", size / (long int) 1e24, unit_char);
+    }
+}
 
 void printStat( char * file ){
     if( file == NULL ) {
@@ -47,14 +75,18 @@ void printStat( char * file ){
 
     struct stat filestat;
 
-    if( stat( file, &filestat ) == 0 )
-    {
-        printf( "File: %s\n", file ); //name
-        printf("File Size: %li bytes\n", filestat.st_size ); //size
-        printf("Access Permissions of file: %s\n", printPermissions(filestat.st_mode)); //permissions
-        printf("Time last accessed: %s\n", ctime( &(filestat.st_atime) )); //time last accessed
-    }
-    else {
+    if ( stat( file, &filestat ) == 0 ) {
+        // name:
+        printf( "File: %s\n", file );
+        // size:
+        char p[256];
+        fmt_SI(filestat.st_size, 'B', p);
+        printf("File Size: %s\n", p);
+        // permissions:
+        printf("Access Permissions of file: %o\n", filestat.st_mode);
+        // time last accessed:
+        printf("Time last accessed: %s\n", ctime( &(filestat.st_atime) ));
+    } else {
         printf("File Error %d: %s\n", errno, strerror(errno));
     }
 }
